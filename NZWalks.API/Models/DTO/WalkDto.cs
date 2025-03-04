57de8b04f500b338b36b6c4fb5d545701d3c05d7
haraiza <humbertoaraiza@gmail.com>
2025-03-04 18:37:43 +0100

    Navigation Properties in Entity

--- NZWalks.API/Models/DTO/WalkDto.cs
+++ NZWalks.API/Models/DTO/WalkDto.cs
@@ -7,7 +7,7 @@
         public required string Description { get; set; }
         public double LengthInKm { get; set; }
         public string? WalkImageURL { get; set; }
-        public Guid DifficultyId { get; set; }
-        public Guid RegionId { get; set; }
+        public RegionDto Region { get; set; }
+        public DifficultyDto Difficulty { get; set; }
     }
 }
--- NZWalks.API/Repositories/SQLWalkRepository.cs
+++ NZWalks.API/Repositories/SQLWalkRepository.cs
@@ -22,7 +22,7 @@ namespace NZWalks.API.Repositories
 
         public async Task<List<Walk>> GetAllAsync()
         {
-            return await dbContext.Walk.ToListAsync();
+            return await dbContext.Walk.Include("Difficulty").Include("Region").ToListAsync();
         }
     }
 }
